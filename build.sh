01f9ef1c3d4986cf454585175da54a241ec8d28b
Jeff Hammond <jeff.science@gmail.com>
2020-11-18 01:26:29 +0000

    build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $HOME
+if [ -f ${HOME}/DPCPP ] ; then
+    cd ${HOME}/DPCPP
+    git fetch --all
+    git checkout sycl
+    git pull
+    git gc
+else
+    git clone https://github.com/intel/llvm.git DPCPP
+fi
+cd ${HOME}/DPCPP
+python ./buildbot/configure.py --cuda
+python ./buildbot/compile.py
+#python ./buildbot/check.py
